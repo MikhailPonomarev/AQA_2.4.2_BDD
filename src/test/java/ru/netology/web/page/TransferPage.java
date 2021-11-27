1f8d16ab605eb2fba592b3510b317bfdff2f62ee
MikhailPonomarev <michailp800@gmail.com>
2021-11-27 16:58:38 +0300

    Added tests, updated transfer page logic

--- src/test/java/ru/netology/web/page/TransferPage.java
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -3,6 +3,7 @@ package ru.netology.web.page;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.Keys;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -11,6 +12,7 @@ public class TransferPage {
     private SelenideElement fromAccountField = $("[data-test-id='from'] input");
     private SelenideElement transferButton = $("[data-test-id='action-transfer']");
     private SelenideElement cancelButton = $("[data-test-id='action-cancel']");
+    private SelenideElement errorNotification = $("[data-test-id='error-notification']");
 
     public TransferPage() {
         amountField.shouldBe(visible);
@@ -28,9 +30,18 @@ public class TransferPage {
         return new DashboardPage();
     }
 
-    public DashboardPage cancelTransaction() {
+    public void falseTransaction(String moneyAmount, String account) {
+        amountField.doubleClick().sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.BACK_SPACE);
+        amountField.setValue(moneyAmount);
+        fromAccountField.doubleClick().sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.BACK_SPACE);
+        fromAccountField.setValue(account);
+        transferButton.click();
+        errorNotification.shouldBe(visible);
+        $(".notification__title").shouldHave(text("Ошибка"));
+    }
+
+    public DashboardPage cancelTransferPage() {
         cancelButton.click();
         return new DashboardPage();
     }
-//TODO: дописать логику
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,6 +1,9 @@
 package ru.netology.web.test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
@@ -11,8 +14,8 @@ import ru.netology.web.page.VerificationPage;
 import static com.codeborne.selenide.Selenide.*;
 
 class MoneyTransferTest {
-    private String firstAccount = "5559 0000 0000 0001";
-    private String secondAccount = "5559 0000 0000 0002";
+    private final String firstAccount = "5559 0000 0000 0001";
+    private final String secondAccount = "5559 0000 0000 0002";
 
     @BeforeEach
     public void setUp() {
@@ -27,33 +30,111 @@ class MoneyTransferTest {
     }
 
     @Test
+    @DisplayName("Various transactions between cards")
     public void transactionsBetweenCards() {
         var dashboardPage = new DashboardPage();
 
-        int firstCardInitBalance = dashboardPage.getFirstCardBalance();
-        int secondCardInitBalance = dashboardPage.getSecondCardBalance();
+        int firstCardBalance = dashboardPage.getFirstCardBalance();
+        int secondCardBalance = dashboardPage.getSecondCardBalance();
 
-        if (firstCardInitBalance == secondCardInitBalance) {
+        if (firstCardBalance == secondCardBalance) {
             dashboardPage.firstCardOpen();
             var transferPage = new TransferPage();
             transferPage.transaction("1000", secondAccount);
-        } else if (firstCardInitBalance > secondCardInitBalance) {
+
+            int firstCardAfterTransaction = dashboardPage.getFirstCardBalance();
+            int expected = firstCardBalance + 1000;
+
+            assertEquals(expected, firstCardAfterTransaction);
+        } else if (firstCardBalance > secondCardBalance) {
             dashboardPage.secondCardOpen();
             var transferPage = new TransferPage();
             transferPage.transaction("5000", firstAccount);
-        } else if (firstCardInitBalance == 0) {
+
+            int secondCardAfterTransaction = dashboardPage.getSecondCardBalance();
+            int expected = secondCardBalance + 5000;
+
+            assertEquals(expected, secondCardAfterTransaction);
+        } else if (firstCardBalance == 0) {
             dashboardPage.firstCardOpen();
             var transferPage = new TransferPage();
             transferPage.transaction("10000", secondAccount);
-        } else if (secondCardInitBalance == 0) {
+
+            int firstCardAfterTransaction = dashboardPage.getFirstCardBalance();
+            int expected = firstCardBalance + 10000;
+
+            assertEquals(expected, firstCardAfterTransaction);
+        } else if (secondCardBalance == 0) {
             dashboardPage.firstCardOpen();
             var transferPage = new TransferPage();
             transferPage.transaction("10000", firstAccount);
+
+            int secondCardAfterTransaction = dashboardPage.getSecondCardBalance();
+            int expected = secondCardBalance + 10000;
+
+            assertEquals(expected, secondCardAfterTransaction);
         } else {
             dashboardPage.firstCardOpen();
             var transferPage = new TransferPage();
-            transferPage.transaction(String.valueOf(secondCardInitBalance), secondAccount);
+            transferPage.transaction(String.valueOf(secondCardBalance), secondAccount);
+
+            int firstCardAfterTransaction = dashboardPage.getFirstCardBalance();
+            int expected = firstCardBalance + secondCardBalance;
+
+            assertEquals(expected, firstCardAfterTransaction);
         }
+    }
+
+    @Test
+    @DisplayName("Should cancel transfer page")
+    public void cancelTransferPage() {
+        var dashboardPage = new DashboardPage();
+        dashboardPage.firstCardOpen();
+        var transferPage = new TransferPage();
+        transferPage.cancelTransferPage();
+
+        dashboardPage.secondCardOpen();
+        transferPage.cancelTransferPage();
+    }
+
+    @Test
+    @DisplayName("Transaction over limit")
+    public void transferOverLimit() {
+        var dashboardPage = new DashboardPage();
+        int firstCardBalance = dashboardPage.getFirstCardBalance();
+        int secondCardBalance = dashboardPage.getSecondCardBalance();
+
+        if (firstCardBalance > secondCardBalance) {
+            dashboardPage.secondCardOpen();
+            var transferPage = new TransferPage();
+            transferPage.transaction(String.valueOf(firstCardBalance + 1), firstAccount);
+
+            int firstCardAfterTransaction = dashboardPage.getFirstCardBalance();
+            int secondCardAfterTransaction = dashboardPage.getSecondCardBalance();
+
+            assertTrue(secondCardAfterTransaction > firstCardAfterTransaction);
+            assertTrue(firstCardAfterTransaction >= 0);
+        } else if (firstCardBalance < secondCardBalance) {
+            dashboardPage.firstCardOpen();
+            var transferPage = new TransferPage();
+            transferPage.transaction(String.valueOf(secondCardBalance + 1), secondAccount);
+
+            int firstCardAfterTransaction = dashboardPage.getFirstCardBalance();
+            int secondCardAfterTransaction = dashboardPage.getSecondCardBalance();
+
+            assertTrue(firstCardAfterTransaction > secondCardAfterTransaction);
+            assertTrue(secondCardAfterTransaction >= 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Transaction with false account")
+    public void falseAccountTransaction() {
+        var dashboardPage = new DashboardPage();
+        dashboardPage.firstCardOpen();
+        var transferPage = new TransferPage();
+        String falseAccount = "5559 0000 0000 0003";
+        transferPage.falseTransaction("1000", falseAccount);
     }
 }
 
